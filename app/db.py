e742f917a03d026e8557865196f26afbc373b941
Farhan <farhan@example.com>
2025-11-24 16:01:10 +0530

    Add gunicorn

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
-    raise Exception("MONGO_URI not found in .env file")
+    raise Exception("mongodb+srv://commerce:commerce@cluster0.9sufkgb.mongodb.net/")
 
 # MongoDB client setup
 from pymongo import MongoClient
